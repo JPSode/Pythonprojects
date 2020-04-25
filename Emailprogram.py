32780f0cca523a11fff47952b0eff12637ade348
JPSode <61152858+JPSode@users.noreply.github.com>
2020-04-25 08:06:39 +0200

    Delete Emailprogram.py

--- Emailprogram.py
+++
@@ -1,7 +0,0 @@
-import smtplib
-conn = smtplib.SMTP('smtp.gmail.com', 587)
-conn.ehlo()
-conn.starttls()
-conn.login('john.sodermark@gmail.com','Programvara1')
-conn.sendmail('john.sodermark@gmail.com','Kait.Sodermark@gmail.com',"Subject:Hej!..\n\nHej,\nMy computer sent this automatically, even though you don't think it's cool..\n\n-JP")
-conn.quit()
